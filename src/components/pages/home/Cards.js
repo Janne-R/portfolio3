79e2d48209de199a1477646e088ba2817d6f156c
Janne-R <74651035+Janne-R@users.noreply.github.com>
2022-10-10 22:11:01 +0200

    Done

--- src/components/pages/home/Cards.js
+++ src/components/pages/home/Cards.js
@@ -12,7 +12,7 @@ const Cards = () => {
   return (
     <Grid>
       <Card image="images/image01.webp" title="Project exam 2" description="Holidaze is a fictional hotel website located in Bergen. This project was created to showcase what I have learned over the past two years. This was my final exam for my studies in front end delevopment." githubLink="https://github.com/Noroff-FEU-Assignments/project-exam-2-Janne-R" />
-      <Card image="images/image03.webp" title="Semester project" description="Code stack is a fictional wikipage for front end development. This project was part of my semester project for the second year." githubLink="https://github.com/Janne-R/semester-project-2-new" />
+      <Card image="images/image03.webp" title="Semester project 2" description="Code stack is a fictional wikipage for front end development. This project was part of my semester project for the second year." githubLink="https://github.com/Janne-R/semester-project-2-new" />
       <Card image="images/image02.webp" title="JavaScript Frameworks CA" description="This project was part of the frameworks course." githubLink="https://github.com/Noroff-FEU-Assignments/js-frameworks-course-assignment-Janne-R" />
     </Grid>
   )
--- src/components/pages/home/Header.js
+++ src/components/pages/home/Header.js
@@ -1,8 +1,27 @@
+import styled from "styled-components";
 
+const H1 = styled.h1`
+text-transform: uppercase;
+`;
+
+const P = styled.p`
+text-transform: uppercase;
+margin-left: 30px;
+margin-top: -10px;
+`;
+
+const Hr = styled.hr`
+  border: 1px solid cadetblue;
+  margin-bottom: 50px; 
+`;
 
 const Header = () => {
   return (
-    <h1>Portfolio Janne Ringdal</h1>
+    <div>
+      <H1>Janne Ringdal</H1>
+      <P> - Junior front-end developer</P>
+      <Hr />
+    </div>
   )
 }
 
