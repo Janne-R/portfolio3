f738ff4d9eeab4932afb494c73268aaf328dbc07
Janne-R <74651035+Janne-R@users.noreply.github.com>
2022-10-10 20:46:09 +0200

    cards

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -1,25 +1,24 @@
 import styled from "styled-components";
-import BackgroundImage from "../common/BackgroundImage";
 
 const Image = styled.img`
     width: 100%;
     object-fit: contain;
 `;
 
-
+const CardContainer = styled.div`
+    padding: 10px;
+`;
 
 const Card = ({ title, image, description, githubLink }) => {
   return (
-    <div>
-
+    <CardContainer>
       <Image src={image} />
-
       <h2>{title}</h2>
       <p>{description}</p>
       <a href={githubLink}>
         <button>github</button>
       </a>
-    </div>
+    </CardContainer>
   )
 }
 
